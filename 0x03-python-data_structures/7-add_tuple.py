3935b51ff67fa68eddc14776dc9644f941dc8ca5
kevapostol <apostolkevin@gmail.com>
2019-12-17 21:42:15 +0000

    Adds 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    '''Adds 2 tuples.'''
+
+    if len(tuple_b) == 0:
+        tuple_b = (0, 0)
+    elif len(tuple_b) == 1:
+        tuple_b = (tuple_b[0], 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
