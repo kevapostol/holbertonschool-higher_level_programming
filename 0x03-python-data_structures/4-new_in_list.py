926cdaccdc6e8f6725f53b54042e1d2e087ecffd
kevapostol <apostolkevin@gmail.com>
2019-12-17 20:52:39 +0000

    Added to check if the list has values

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,12 +2,12 @@
 def new_in_list(my_list, idx, element):
     '''Replaces an element in a list at a specific position without modifying
 the original list (like in C).'''
+    if my_list:
+        if idx < 0:
+            return my_list
+        elif idx > len(my_list):
+            return my_list
 
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-
-    new_list = my_list.copy()
-    new_list[idx] = element
-    return new_list
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
