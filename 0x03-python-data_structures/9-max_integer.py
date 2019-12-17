d1222bff52e782726602abf16f68f735f39f7e7e
kevapostol <apostolkevin@gmail.com>
2019-12-17 22:33:42 +0000

    Fif my_list:         max = my_list[0]
            for i in my_list:
                if max < i:
                    max = i
    
            return max
    
        else:

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    '''Finds the biggest integer of a list.'''
+
+    if my_list:
+        max = my_list[0]
+
+        for i in my_list:
+            if max < i:
+                max = i
+
+        return max
+
+    else:
+        return None
