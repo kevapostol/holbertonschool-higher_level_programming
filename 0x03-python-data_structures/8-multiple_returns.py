f8170b9535225206f4ff4405c7867e91c38802d2
kevapostol <apostolkevin@gmail.com>
2019-12-17 22:25:47 +0000

    Modified Code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     '''Returns a tuple with the length of a string and its first character.'''
 
     if len(sentence) == 0:
-        return (0, None)
-    return (len(sentence), sentence[0])
+        return len(sentence), None
+    return len(sentence), sentence[0]
