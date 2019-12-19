819fb80b122f96e33f819944935cf9e6cac78c05
kevapostol <apostolkevin@gmail.com>
2019-12-19 21:45:52 +0000

    Square Matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    '''Computes the square value of all integers of a matrix.'''
+
+    new_mtx = [];
+
+    for mtx in matrix:
+        sub = []
+        for ele in mtx:
+            sub.append(ele**2)
+        new_mtx.append(sub)
+
+    return(new_mtx)
