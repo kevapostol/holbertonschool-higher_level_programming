c4521aa8874e7aa920ab472b768e24e215241682
kevapostol <apostolkevin@gmail.com>
2019-12-19 22:08:58 +0000

    Added function description

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    '''Adds all unique integers in a list (only once for each integer).'''
+
     new_list = my_list[:]
     new_list = [(replace if ele == search else ele) for ele in new_list]
     return(new_list)
