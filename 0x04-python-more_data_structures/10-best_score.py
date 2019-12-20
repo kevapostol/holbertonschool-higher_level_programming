f484524145beab5b3a243aa5bffae3f3034db572
kevapostol <apostolkevin@gmail.com>
2019-12-20 03:57:00 +0000

    Reverse the if and else statement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     '''Returns a key with the biggest integer value.'''
 
-    if a_dictionary is None:
-        return None
-    else:
+    if a_dictionary:
         return max(a_dictionary, key=a_dictionary.get)
+    else:
+        return None
