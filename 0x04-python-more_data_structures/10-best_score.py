2419c35a676e0317acb42d7d360ab12dc8aed13c
kevapostol <apostolkevin@gmail.com>
2019-12-20 03:54:53 +0000

    Added an else statement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,4 +4,5 @@ def best_score(a_dictionary):
 
     if a_dictionary is None:
         return None
-    return max(a_dictionary, key=a_dictionary.get)
+    else:
+        return max(a_dictionary, key=a_dictionary.get)
