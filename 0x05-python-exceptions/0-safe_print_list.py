98ec4768456203127ec2b9cf1f5ed9f3f98aff30
kevapostol <apostolkevin@gmail.com>
2020-01-08 21:49:14 +0000

    Modified code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,11 +8,8 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end='')
                 count += 1
             print()
-    except TypeError:
-        if my_list:
-            print()
-            return count
     except IndexError:
-        if my_list:
-            print()
-            return count
+        print()
+        return count
+    finally:
+        return count
