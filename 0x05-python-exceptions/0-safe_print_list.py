6cffb01ff47493abe3a375c88f9a4586ffcc6fea
kevapostol <apostolkevin@gmail.com>
2020-01-08 21:45:34 +0000

    Modified code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,8 @@ def safe_print_list(my_list=[], x=0):
     except TypeError:
         if my_list:
             print()
+            return count
     except IndexError:
         if my_list:
             print()
-    return count
+            return count
