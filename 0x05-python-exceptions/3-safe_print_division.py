8098988465a19b3dbb0a9058f4c22124c66df20d
kevapostol <apostolkevin@gmail.com>
2020-01-09 00:01:28 +0000

    Modified the print funciton

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,9 +4,9 @@ def safe_print_division(a, b):
 
     try:
         result = a / b
-        print("Inside result: {:d}".format(result))
+        print("Inside result: {}".format(result))
     except:
         result = None
-        print("Inside result: None")
+        print("Inside result: {}".format(result))
     finally:
         return result
