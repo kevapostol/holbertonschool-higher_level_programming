8fcf6b55e98f64f25c683cf2b299f09c298a09a7
kevapostol <apostolkevin@gmail.com>
2020-01-10 04:00:43 +0000

    Prints the position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if isinstance(value, tuple) is False or isinstance(value[0], int) is\
+            False or isinstance(value[1], int) is False or value[0] < 0 or\
+                value[1] < 0:
+            raise ValueError('position must be a tuple of 2 positive integers')
+        else:
+            self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                if self.__position[1] > 0:
+                    pass
+                else:
+                    for m in range(self.__position[0]):
+                        print(' ', end='')
+
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
+
+    def area(self):
+        return self.__size ** 2
