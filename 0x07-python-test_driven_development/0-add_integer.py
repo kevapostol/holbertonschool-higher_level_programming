f1e4d879d65c63707ae0e5013fd5170c520bbc41
kevapostol <apostolkevin@gmail.com>
2020-01-15 00:57:31 +0000

    Modified code for adding more edge cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,10 +10,14 @@ def add_integer(a, b=98):
     '''
     The function for adding 2 integers
     '''
-    if isinstance(a, int) is False and isinstance(a, float) is False:
+    if isinstance(a, int) is False and isinstance(a, float) is False or a != a:
         raise TypeError('a must be an integer')
-    if isinstance(b, int) is False and isinstance(b, float) is False:
+    if isinstance(b, int) is False and isinstance(b, float) is False or b != b:
         raise TypeError('b must be an integer')
+    if a == float('inf'):
+        a = 0
+    if b == float('inf'):
+        b = 0
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
