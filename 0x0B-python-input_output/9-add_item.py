fc5059c39d5ad77d4b9d67942058ebe27e7d2254
kevapostol <apostolkevin@gmail.com>
2020-01-23 00:35:37 +0000

    Added try except

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,15 +10,18 @@ import json
 from sys import argv
 '''Imports the json and argv module'''
 
-if len(argv) is 1:
-    with open('add_item.json', mode='a', encoding='utf-8') as a_file:
+try:
+    with open('add_item.json', mode= 'r', encoding='utf-8') as a_file:
+        pass
+except:
+    with open('add_item.json', mode= 'w', encoding='utf-8') as a_file:
         a_file.write('[]')
+finally:
+    if len(argv) > 1:
+        for val_str in range(1, len(argv)):
+            with open('add_item.json', mode='r', encoding='utf-8') as a_file:
+                obj = json.load(a_file)
+                obj.append(argv[val_str])
 
-if len(argv) > 1:
-    for val_str in range(1, len(argv)):
-        with open('add_item.json', mode='r', encoding='utf-8') as a_file:
-            obj = json.load(a_file)
-            obj.append(argv[val_str])
-
-        with open('add_item.json', mode='w', encoding='utf-8') as a_file:
-            json.dump(obj, a_file)
+            with open('add_item.json', mode='w', encoding='utf-8') as a_file:
+                json.dump(obj, a_file)
