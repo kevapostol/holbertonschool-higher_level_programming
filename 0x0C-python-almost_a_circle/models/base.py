f2be221c33a7502532847cc54469d29f5c060816
kevapostol <apostolkevin@gmail.com>
2020-01-27 20:35:32 +0000

    Pep8 Comp

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 '''
 The Base module
 '''
+import json
+
 
 class Base:
     '''
@@ -16,3 +18,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Returns the JSON string representation of list_dictionaries'''
+        if list_dictionaries is None:
+            return '[]'
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Writes the JSON string representation of list_objs to a file'''
+        print(list_objs.to_dictionary)
