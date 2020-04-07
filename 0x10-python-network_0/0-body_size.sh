d7685c2f5c361eefd3178d7b1bc4e41d26865a5f
kevapostol <apostolkevin@gmail.com>
2020-04-07 21:03:29 +0000

    Updated file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s "$1" | wc -c
+# a Bash script that takes in a URL and prints the bytes
+curl --silent "$1" | wc -c
