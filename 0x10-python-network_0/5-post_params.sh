8af229cb0e0233034571b06d5aa60c623ee19ff2
kevapostol <apostolkevin@gmail.com>
2020-04-07 22:03:43 +0000

    a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" 0.0.0.0:5000/route_6
