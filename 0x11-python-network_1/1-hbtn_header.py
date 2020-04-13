bacd22f9602bfab8fd04982d2fc8994be2aeec64
kevapostol <apostolkevin@gmail.com>
2020-04-13 20:04:19 +0000

    Trying the key

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,4 +9,5 @@ import urllib.request
 if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
-        print(response.getheader("X-Request-Id"))
+        the_page = response.info()
+        print(the_page['X-Request-Id'])
