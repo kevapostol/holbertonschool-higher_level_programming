4897d4bcc60cb680bc6d68a145442bc984dafc09
kevapostol <apostolkevin@gmail.com>
2020-04-13 22:55:14 +0000

    Task 8 json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter and
+sends a POST request to
+http://0.0.0.0:5000/search_user with the
+letter as a parameter
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        val = ""
+    else:
+        val = argv[1]
+        response = requests.post(sys.argv[1], data={'q': val})
+        deserialized_json = response.json()
+
+        if response.request.headers['Content-Type'] == 'application/json':
+            print("Not a valid JSON")
+        else if len(deserialized_json) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(deserialized_json.get("id"),
+                                   deserialized_json.get("name")))
