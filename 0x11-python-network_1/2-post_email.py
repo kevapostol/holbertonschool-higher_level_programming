42f1329a338e3832152373ee80df9e88725659aa
kevapostol <apostolkevin@gmail.com>
2020-04-13 20:34:28 +0000

    Modified code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,12 +2,12 @@
 # a Python script that takes in a URL and an email,
 # sends a POST request to the passed URL with the
 # email as a parameter, and displays the body of the response
-import sys
+from sys import argv
 import urllib.parse
 import urllib.request
 
 url = sys.argv[1]
-values = {'email': sys.argv[2]}
+values = {'email': argv[2]}
 data = urllib.parse.urlencode(values)
 data = data.encode('ascii')
 req = urllib.request.Request(url, data)
