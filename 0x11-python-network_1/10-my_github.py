6b6e97eca1018b7b367ae76b670fc27e1a45ca5b
kevapostol <apostolkevin@gmail.com>
2020-04-14 00:12:12 +0000

    a Python script that takes your Github credentials (username and password) and uses the Github API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+a Python script that takes your Github credentials
+(username and password) and uses the Github API to
+display your id
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    request = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    print('{}'.format(request.json().get('id')))
