be1815ab75786ed96bcfa8c7b4ed878650f11cc6
kevapostol <apostolkevin@gmail.com>
2020-05-05 20:12:10 +0000

    Refactored codebase

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,12 +5,12 @@ Searches the second biggest integer in the list of arguments
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let li_nums = [];
+  let liNums = [];
 
   for (let i = 2; i < process.argv.length; i += 1) {
-    li_nums.push(parseInt(process.argv[i]));
+    liNums.push(parseInt(process.argv[i]));
   }
 
-  li_nums = li_nums.sort((a, b) => b - a);
-  console.log(li_nums[1]);
+  liNums = liNums.sort((a, b) => b - a);
+  console.log(liNums[1]);
 }
