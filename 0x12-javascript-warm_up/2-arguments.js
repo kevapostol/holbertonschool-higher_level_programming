1707dac279ddefd79dbd32edb683d6cc4f2a8f68
kevapostol <apostolkevin@gmail.com>
2020-05-05 17:24:55 +0000

    Refactored Code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 /*
 Prints a message depending of the number of arguments passed
 */
-if (process.argv.length <= 1) {
+if (process.argv.length <= 2) {
   console.log('No argument');
-} else {
+} else if (process.argv.length === 3){
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
