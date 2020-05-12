a3f214437ca5638ddf9a0a8ba9332537c1f898bc
kevapostol <apostolkevin@gmail.com>
2020-05-12 20:03:07 +0000

    Modified error output

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const uri = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 request.get(uri, (error, response, body) => {
   if (error) {
-    console.log(error);
+    error.log(error);
   } else if (response.statusCode === 200) {
     const obj = JSON.parse(body);
     console.log(obj.title);
