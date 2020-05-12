cbb9e420d3ee25e11dadf8203bc9012712a8bd31
kevapostol <apostolkevin@gmail.com>
2020-05-12 01:52:48 +0000

    Modified codebase to not use reverse method

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  let newList;
+  for (let i = list.legth - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
 };
