983f423c00dfebc31a431998cc9fb71e9e570ff9
kevapostol <apostolkevin@gmail.com>
2020-05-12 17:23:37 +0000

    Fixed semistandard warning

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-var fs = require("fs");
-fs.readFile(process.argv[2], "utf-8", (err, data) => {
-    if (err) { console.log(err) }
-    console.log(data);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) { console.log(err); }
+  console.log(data);
 });
