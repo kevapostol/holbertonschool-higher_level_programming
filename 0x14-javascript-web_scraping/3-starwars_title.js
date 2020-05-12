1362559a5d52614509507ded7d8c7bd779d85bc3
kevapostol <apostolkevin@gmail.com>
2020-05-12 20:03:51 +0000

    Brought back console.log

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const uri = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 request.get(uri, (error, response, body) => {
   if (error) {
-    error.log(error);
+    console.log(error);
   } else if (response.statusCode === 200) {
     const obj = JSON.parse(body);
     console.log(obj.title);
